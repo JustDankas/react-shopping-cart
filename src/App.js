9bab05f1ea658be065c51889e807bc33e3156696
JustDankas <jan-haspel@hotmail.com>
2022-04-26 15:05:33 +0300

    create product component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
 //Feature 1
-import React from "react";
+import React, {Component} from "react";
+import Products from "./Components/Products";
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All rights is reserved
-      </footer>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products: data.products,
+      size:"",
+      sort:"",
+    }
+  }
+  render() { 
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All rights is reserved
+        </footer>
+      </div>
+      );
+  }
 }
-
+ 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="grid-container">
+//       <header>
+//         <a href="/">React Shopping Cart</a>
+//       </header>
+//       <main>
+//         <div className="contnet">
+//           <div className="main">
+
+//           </div>
+//           <div className="sidebar">
+
+//           </div>
+//         </div>
+//       </main>
+//       <footer>
+//         All rights is reserved
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default App;
